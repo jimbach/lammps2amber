6d2e904e4a3c338f066f8a7b53de5a5ca657804e
Jim Bachmann <j_bach04@uni-muenster.de>
2026-06-03 15:55:38 +0200

    added output of missing parameters

--- uff4mof_to_amber.sh
+++ uff4mof_to_amber.sh
@@ -16,24 +16,28 @@ antechamber -i $1.pdb -fi pdb -o $1.mol2 -fo mol2 -j 5 -at sybyl -dr no
 #replace the molname
 sed -i "s/UNL/$shortname/g" $1.mol2
 
-${LAMMPS2AMBERHOME}/tools/rename_mol2.py $1.data $1.mol2 renamed.mol2
+./rename_mol2.py $1.data $1.mol2 renamed.mol2
 mv renamed.mol2 $1.mol2
 
 
-${LAMMPS2AMBERHOME}/tools/copy_bonds_to_mol2.py $1.mol2 $1.data out.mol2
+./copy_bonds_to_mol2.py $1.mol2 $1.data out.mol2
 mv out.mol2 $1.mol2
 
 
-${LAMMPS2AMBERHOME}/tools/make_bigger_unitcell.py $1.mol2 $1.data out.mol2
+./make_bigger_unitcell.py $1.mol2 $1.data out.mol2
 mv out.mol2 $1.mol2
 
-${LAMMPS2AMBERHOME}/tools/uniquely_label_atoms_mol2.py $1.mol2 out.mol2
+./uniquely_label_atoms_mol2.py $1.mol2 out.mol2
 mv out.mol2 $1.mol2
 
-${LAMMPS2AMBERHOME}/tools/gen_frcmod_template.py $1.mol2 $shortname.frcmod
 
 
-${LAMMPS2AMBERHOME}/tools/fill_frcmod2.py $1.data $shortname.frcmod filled.frcmod 
+#parmchk -i $1.mol2 -f mol2 -o $1.frcmod -s uff -a Y
+
+./gen_frcmod_template.py $1.mol2 $shortname.frcmod
+
+
+./fill_frcmod2.py $1.data $shortname.frcmod filled.frcmod 
 mv filled.frcmod $shortname.frcmod
 
 #grep -v 0.0000 $shortname.frcmod > removed.frcmod
@@ -49,6 +53,26 @@ boxz=$(grep "zlo zhi" $1.data | awk '{print $2}')
 
 
 filename="$1"
+
+grep -v 0.0000 ZIF.frcmod > nozero.frcmod
+
+echo "building a tleap file"
+echo "#################################################"
+tee $filename.tleap.in << END
+source leaprc.uff
+loadamberparams nozero.frcmod
+$shortname = loadmol2 $filename.mol2
+set $shortname box {$boxx $boxy $boxz}
+saveoff $shortname $shortname.lib 
+loadoff $shortname.lib
+saveamberparm $shortname $filename.prmtop $filename.inpcrd
+quit
+END
+echo "#################################################"
+echo "running tleap"
+tleap -f $filename.tleap.in | awk '!seen[$0]++' | grep " No " > MISSING-PARAMTERS.dat
+
+rm nozero.frcmod
 
 echo "building a tleap file"
 echo "#################################################"
